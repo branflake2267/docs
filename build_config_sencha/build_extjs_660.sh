b7e5ef2508d22fbb484ad6883d2bf7b78f5e9ada
Brandon Donnelson <branflake2267@gmail.com>
2018-06-30 15:17:11 -0700

    Improve the test bash script

--- build_config_sencha/build_extjs_660.sh
+++ build_config_sencha/build_extjs_660.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # build extjs docs
 
+# docs config directory 
+CONFIG_DIR="$(cd ../build_config_sencha; echo "$(pwd)")"
+
 # Run this from the library directory where the app logic is.
 cd ../lib
 
+# docs build command
 node --max-old-space-size=4076 index create-app-html \
---buildConfigsDir=~/git/docs/build_config_sencha \
+--buildConfigsDir=$CONFIG_DIR \
 --product=extjs \
 --version=6.6.0 \
 --syncRemote=true \

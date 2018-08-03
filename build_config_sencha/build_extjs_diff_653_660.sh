ed59b22bba3442b0b145cc12c7cfa92cdec7b8de
Brandon Donnelson <branflake2267@gmail.com>
2018-08-02 21:03:40 -0700

    Rename diff bash script.

--- build_config_sencha/build_extjs_diff_653_660.sh
+++ build_config_sencha/build_extjs_diff_653_660.sh
@@ -6,7 +6,7 @@ cd ../lib
 # docs config directory 
 CONFIG_DIR="$(cd ../build_config_sencha; echo "$(pwd)")"
 
-# create-diff
+# create-diff (Creates both json and markdown)
 node --max-old-space-size=4076 index create-diff \
 --buildConfigsDir=$CONFIG_DIR \
 --diffTarget=extjs \
@@ -14,11 +14,12 @@ node --max-old-space-size=4076 index create-diff \
 --diffSource=extjs \
 --diffSourceVersion=6.5.3
 
-# create-diff-md
+
+# create-diff-md - (Creates markdown)
 # node --max-old-space-size=4076 index create-diff-md \
 # --buildConfigsDir=$CONFIG_DIR \
 # --product=extjs \
-# --new=6.5.3 \
-# --old=6.5.2 \
+# --new=6.6.0 \
+# --old=6.5.3 \
 # --newFile="$CONFIG_DIR/build/input/extjs/6.5.3/all-classes/modern_all-classes.json" \
 # --oldFile="$CONFIG_DIR/build/input/extjs/6.5.2/all-classes/modern_all-classes.json"

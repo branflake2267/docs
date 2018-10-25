d0c43abe7f9382e9de1e120567cd7856ee8a4966
Brandon Donnelson <branflake2267@gmail.com>
2018-10-25 14:22:51 -0700

    Refactor assets directory. Fix assetsDir paths.

--- build_config_sencha/build_cmd_660.sh
+++ build_config_sencha/build_cmd_660.sh
@@ -13,10 +13,10 @@ CONFIG_DIR="$(cd ../build_config_sencha; echo "$(pwd)")"
 # docs build command
 node --max-old-space-size=4076 index create-app-html \
 --buildConfigsDir=$CONFIG_DIR \
---product=extjs \
---version=6.6.0-CE \
+--product=cmd \
+--version=6.6.0 \
 --syncRemote=true \
 --forceDoxi=true \
 --log \
---production
+--production 
 

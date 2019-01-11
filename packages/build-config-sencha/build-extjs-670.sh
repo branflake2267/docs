1281c188c1a364c37ada71adc777aed56c0d5fae
Eric-revollo <39591108+Eric-revollo@users.noreply.github.com>
2019-01-11 08:44:15 -0400

    Update build-extjs-670.sh

--- packages/build-config-sencha/build-extjs-670.sh
+++ packages/build-config-sencha/build-extjs-670.sh
@@ -11,7 +11,7 @@ CONFIG_DIR="$(pwd)"
 npx sencha-docs-generator  create-app-html \
 --buildConfigsDir=$CONFIG_DIR \
 --product=extjs \
---version=6.7.0 \
+--version=6.7.0-EA \
 --syncRemote=true \
 --forceDoxi=true \
 --log \

bd2a9d555556eb7b86c1df8bbec303f1475cacf0
Eric-revollo <39591108+Eric-revollo@users.noreply.github.com>
2019-01-16 09:05:27 -0400

    Create build-cmd-670.sh

---
+++ packages/build-config-sencha/build-cmd-670.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# build extjs docs
+
+# clean the build directory
+rm -rf ./build
+
+# docs config directory 
+CONFIG_DIR="$(pwd)"
+
+# docs build command
+npx sencha-docs-generator create-app-html \
+--buildConfigsDir=$CONFIG_DIR \
+--product=cmd \
+--version=6.7.0 \
+--syncRemote=true \
+--forceDoxi=true \
+--log \
+--production

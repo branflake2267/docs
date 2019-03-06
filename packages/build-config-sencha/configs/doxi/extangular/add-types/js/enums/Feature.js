35c3d6f05be356a1c94ca4425b9405132d928e02
Eric-revollo <39591108+Eric-revollo@users.noreply.github.com>
2019-03-06 15:43:54 -0400

    add types

---
+++ packages/build-config-sencha/configs/doxi/extangular/add-types/js/enums/Feature.js
@@ -0,0 +1,8 @@
+//
+// Definitions of enums referenced in documentation.
+//
+
+/**
+ * @enum [Ext.enums.Feature=feature.*]
+ * Enumeration of all ftypes.
+ */
---
+++ packages/build-config-sencha/configs/doxi/extangular/add-types/js/enums/Plugin.js
@@ -0,0 +1,8 @@
+//
+// Definitions of enums referenced in documentation.
+//
+
+/**
+ * @enum [Ext.enums.Plugin=plugin.*]
+ * Enumeration of all ptypes.
+ */

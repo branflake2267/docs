02a7512f3e21667e26cf1749e650348bf4e4e83c
Eric-revollo <39591108+Eric-revollo@users.noreply.github.com>
2019-04-02 13:40:52 -0400

    Update index.js

--- packages/docs-generator/modules/create-rext-app-html/index.js
+++ packages/docs-generator/modules/create-rext-app-html/index.js
@@ -653,10 +653,10 @@ class OpenToolingHtmlApp extends HtmlApp {
 
             // set the config and property names to match what React users would expect
             if (prepared.configs) {
-                prepared.configs.name    = 'properties';
+                prepared.configs.name    = 'config';
             }
             if (prepared.properties) {1
-                prepared.properties.name = 'fields';
+                prepared.properties.name = 'property';
             }
 
             // if there are events on the class camelize them and prefix with 'on' to
